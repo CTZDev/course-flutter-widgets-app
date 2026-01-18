62f41fa5a5399ff086fbcb893fedfe4c1c1b77df
CTZDev <carlosteranzavaleta@hotmail.com>
2026-01-17 20:53:01 -0500

    feat: Set up initial Flutter application with a basic UI and custom theme definition.

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const List<Color> colors = [
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+    Colors.deepPurple,
+    Colors.pink,
+    Colors.red,
+    Colors.orange,
+    Colors.amber,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.cyan,
+  ];
+
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+    : assert(
+        selectedColor >= 0 && selectedColor < colors.length,
+        'selectedColor must be between 0 and ${colors.length - 1}',
+      );
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(seedColor: colors[selectedColor]),
+  );
+}
